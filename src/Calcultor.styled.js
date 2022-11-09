1d568782a3bf5ab6930d2e222220769c09aca158
zessadqu <zikoessad@gmail.com>
2022-11-10 00:03:01 +0100

    rigt

--- src/Calcultor.styled.js
+++ src/Calcultor.styled.js
@@ -8,12 +8,11 @@ margin-top:0px;
 top: 50%;
 left:50%;
 transform: translate(-50%,-50%);
-border-radius: 5%;
 border: 5px;
 background-color:lightcyan;
 overflow: hidden;
 box-shadow: 10px 10px 10px grey;
-@media only screen and (max-width: 600px) {
+@media screen and (max-height: 700px) {
 height: 100%;
 width: 100%;
 margin-top:0px;
@@ -36,9 +35,9 @@ overflow-x: hidden;
 display: grid;
 grid-template-columns: repeat(4, 1fr);
 background:linear-gradient(315deg, #485461 0%, #28313b 74%);
-@media only screen and (max-width: 600px) {
+@media  screen and (max-height: 700px) {
 width: 100%;
-height: calc(100vh - 100px);
+height: 600px;
 }
 ;
 `;
@@ -48,12 +47,13 @@ export const ButtonIn = styled.input`
  border-radius:30% ;
  font-size: larger;
  text-align: center;
- background-color: ${props => props.value === '='? 'blue':props.value ==='+'?'orange':props.value ==='-'?'cyan':props.value ==='*'?'red':props.value ==='Del'?'purple':'rgb(80 60 209)' };
+ background-color: ${props => props.value === '='? 'lightblue':props.value ==='+'?'orange':props.value ==='-'?'cyan':props.value ==='*'?'red':props.value ==='Del'?'purple':'rgb(80 60 209)' };
  :hover{
     background-color: #454545;
    }
    transform: scale(1);
-   @media screen {
-      width: calc(450px/4);
+   @media screen and (max-height: 700px ) {
+      width: calc(300px/4);
+      height: calc(500px/5);
    }
  `
